3b4ec5e4e2bacd8dab034dd507f066f049dceb95
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-11 19:04:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,7 +11,6 @@ def won?(board)
 WIN_COMBINATIONS.each do |combo|
     if board[combo[0]] == 'X' && board[combo[1]] == 'X' && board[combo[2]] == 'X'
       return combo
-    else
     end
   end
   return false
