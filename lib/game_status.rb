3f3c6cec37d756fddd19d4ddabd550c20541fbd8
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-11 18:58:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,13 +9,10 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
 def won?(board)
   wincombo=[]
 WIN_COMBINATIONS.each do |combo|
-  combo.each do |index|
-    if board[index] == 'X'
-      wincombo.push(index)
-  else
+    if board[combo[0]] == 'X' && board[combo[1]] == 'X' && board[combo[2]] == 'X'
+      return combo
+    else
     false
+    end
   end
-  end
-end
-wincombo
 end
